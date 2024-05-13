2a0fcc1e246eeec62961187bb6d89c9741d1d5ee
daniajaison13 <daniajaison@gmail.com>
2024-05-12 21:52:07 -0700

    Updated the code for deployment

--- dev/src/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.models.user import User
-from app.models.game import Game
-from app.models.game_history import GameHistory
-from app.models.leaderboard import Leaderboard
--- dev/src/client/src/setupTests.js
+++
@@ -1,9 +0,0 @@
-// setupTests.js
-import "@testing-library/jest-dom";
-import fetchMock from "jest-fetch-mock";
-import "@testing-library/user-event";
-import "@testing-library/jest-dom/extend-expect";
-
-fetchMock.enableMocks();
-
-// Any other global setup needed for your tests
